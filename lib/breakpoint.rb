b65c1c0fbfd75d939885e9988d405d3937b05570
Sam Richard <sam.richard@weforum.org>
2012-05-24 15:27:17 -0400

    Updated rb, included Fraction gem and numerator/denominator functions
    Fraction gem needed for -o-device-pixel-ratio. Sass Script for numerator/denominator functions.

--- lib/breakpoint.rb
+++ lib/breakpoint.rb
@@ -1,9 +1,22 @@
 require 'compass'
+require 'fraction'
+
+# Connect Fraction up to Sass so we can use it for fracking use it for -o-device-pixel-ratio
+module Sass::Script::Functions
+ def numerator(number)
+   Sass::Script::Number.new number.value.fraction.first
+ end
+ def denominator(number)
+      num, den = number.value.fraction
+      Sass::Script::Number.new(den)
+ end
+end
+
 Compass::Frameworks.register("breakpoint", :path => "#{File.dirname(__FILE__)}/..")
 
 module Breakpoint
 
-  VERSION = "0.1"
-  DATE = "2012-05-22"
+  VERSION = "0.2"
+  DATE = "2012-05-24"
 
 end
