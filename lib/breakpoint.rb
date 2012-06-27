159cd48d05b8ff216eb450f2a570a13f3d9bef78
Sam Richard <snugug@gmail.com>
2012-06-27 15:57:44 -0400

    Updated Breakpoint RB

--- lib/breakpoint.rb
+++ lib/breakpoint.rb
@@ -3,6 +3,6 @@ require 'compass'
 Compass::Frameworks.register("breakpoint", :path => "#{File.dirname(__FILE__)}/..")
 
 module Breakpoint
-  VERSION = "1.0"
-  DATE = "2012-06-22"
+  VERSION = "1.0.1"
+  DATE = "2012-06-27"
 end
