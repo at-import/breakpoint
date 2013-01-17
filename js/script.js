d940c82449748dac33c57add0e78f084bcef1c41
Mason Wendell <mason@canarypromo.com>
2013-01-17 00:35:41 -0500

    fix ipad label tapping. css specificity fix

--- js/script.js
+++ js/script.js
@@ -36,3 +36,17 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('details').details();
 });
+
+// fix label taps for ipad 1
+if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) {
+  $(document).ready(function () {
+    $('label[for]').click(function () {
+      var el = $(this).attr('for');
+      if ($('#' + el + '[type=radio], #' + el + '[type=checkbox]').attr('selected', !$('#' + el).attr('selected'))) {
+        return;
+      } else {
+        $('#' + el)[0].focus();
+      }
+    });
+  });
+}
