4dfd020cb3b9c890a9d4dee85fcabc89b67147e4
Mason Wendell <mason@canarypromo.com>
2013-01-17 23:30:30 -0500

    new tabs, a few layout changes, a few bug fixes

--- js/script.js
+++ js/script.js
@@ -2,33 +2,19 @@
 *  [your name here]
 */
 
-// fix heights and spacing on tab groups
-$(document).ready(function(){
-  $('html').addClass('tabs-processed');
-  $tabs = $('.tabs');
-  $padding_difference = 28;
-  $('.tabs .content').css('bottom', 'auto');
-  function fix_tabs_min_height(tabs) {
-    $these_tabs = tabs;
-    $these_tabs.find('.content').hide();
-    $this_checked_content = $these_tabs
-      .find('[type=radio]:checked')
-      .siblings('.content');
-    $this_checked_content.show();
-    $this_height = $this_checked_content.height();
-    $these_tabs
-      .css('min-height', $this_height + $padding_difference);
+// add a class to style the active tabs
+$(document).ready(function() {
+  function set_tabset(tabset) {
+    $tabset = tabset;
+    $checked_id = $tabset.find('[type=radio]:checked').attr('id');
+    $tabset.find('label').removeClass('radio-checked');
+    $tabset.find('label[for='+ $checked_id +']').addClass('radio-checked');
   }
-  $tabs.each(function(index, el){
-    fix_tabs_min_height($(el));
-    $(this).find('[type=radio]').change(function() {
-      fix_tabs_min_height($(el));
-    });
+  $('.tabset').each(function(index, el) {
+    set_tabset($(this));
   });
-  $(window).resize(function() {
-    $tabs.each(function(index, el){
-      fix_tabs_min_height($(el));
-    });
+  $('[type=radio]').change(function() {
+    set_tabset($(this).parents('.tabset'));
   });
 });
 
