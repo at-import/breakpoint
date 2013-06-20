066b433ea90cc4f2def39557239793edd6c2b860
James Reggio <james.reggio@gmail.com>
2013-06-20 15:25:01 -0700

    Remove unnecessary Compass compilation settings

--- test/config.rb
+++ test/config.rb
@@ -1,6 +1,5 @@
 # File system locations
 sass_dir              = 'sass'
 css_dir               = 'test'
-images_dir            = 'images'
-javascripts_dir       = 'js'
+# Import latest breakpoint library
 add_import_path       '../stylesheets'
