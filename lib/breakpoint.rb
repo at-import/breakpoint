461b158123756fe209eee3323edf73521f8376c0
Sam Richard <snugug@gmail.com>
2013-12-04 15:55:42 -0500

    Version Bump

--- lib/breakpoint.rb
+++ lib/breakpoint.rb
@@ -3,6 +3,6 @@ require 'compass'
 Compass::Frameworks.register("breakpoint", :path => "#{File.dirname(__FILE__)}/..")
 
 module Breakpoint
-  VERSION = "2.3.0"
-  DATE = "2013-12-02"
+  VERSION = "2.3.1"
+  DATE = "2013-12-04"
 end
