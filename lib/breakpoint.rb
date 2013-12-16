88e6c69e61223908a0016dd837528598b437593f
Sam Richard <snugug@gmail.com>
2013-12-16 16:57:19 -0500

    Sassy Maps (soft) dependency
    Memoization taking place! This is a soft dependency, checks are in place to ensure that memoization only fires if Sassy Maps is available

--- lib/breakpoint.rb
+++ lib/breakpoint.rb
@@ -1,8 +1,9 @@
 require 'compass'
+require 'sassy-maps'
 
 Compass::Frameworks.register("breakpoint", :path => "#{File.dirname(__FILE__)}/..")
 
 module Breakpoint
-  VERSION = "2.3.1"
-  DATE = "2013-12-04"
+  VERSION = "2.3.2"
+  DATE = "2013-12-16"
 end
