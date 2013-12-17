e46adabe50885ce6c233145a1442202a5b8e67d7
Sam Richard <snugug@gmail.com>
2013-12-17 10:28:20 -0500

    Version bump

--- lib/breakpoint.rb
+++ lib/breakpoint.rb
@@ -4,6 +4,6 @@ require 'sassy-maps'
 Compass::Frameworks.register("breakpoint", :path => "#{File.dirname(__FILE__)}/..")
 
 module Breakpoint
-  VERSION = "2.3.2"
-  DATE = "2013-12-16"
+  VERSION = "2.4.0"
+  DATE = "2013-12-17"
 end
