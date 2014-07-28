952db22b7ad8921f27f6464f6944547c95ea4868
Sam Richard <snugug@gmail.com>
2014-07-28 10:17:37 -0400

    Resolves #108. Added to Sass Path

--- lib/breakpoint.rb
+++ lib/breakpoint.rb
@@ -1,12 +1,16 @@
+breakpoint_path = File.expand_path('../stylesheets', __FILE__)
+
 if (defined? Compass)
   require 'sassy-maps'
   Compass::Frameworks.register(
     "breakpoint",
-    :path => "#{File.dirname(__FILE__)}/.."
+    :stylesheets_directory => breakpoint_path
   )
+else
+  ENV["SASS_PATH"] = [ENV["SASS_PATH"], breakpoint_path].compact.join(File::PATH_SEPARATOR)
 end
 
 module Breakpoint
-  VERSION = "2.4.2"
-  DATE = "2014-03-11"
+  VERSION = "2.4.4"
+  DATE = "2014-07-24"
 end
