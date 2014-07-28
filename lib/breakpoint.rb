68e2c36fc31fd15ba25184eb7e4d32e4f5770ea6
Sam Richard <snugug@gmail.com>
2014-07-28 10:31:00 -0400

    Turns out that doesn't work as wanted, need to do the full path

--- lib/breakpoint.rb
+++ lib/breakpoint.rb
@@ -1,16 +1,17 @@
-breakpoint_path = File.expand_path('../stylesheets', __FILE__)
+breakpoint_sass_path = File.expand_path('../stylesheets', __FILE__)
+breakpoint_path = "#{File.dirname(__FILE__)}/.."
 
 if (defined? Compass)
   require 'sassy-maps'
   Compass::Frameworks.register(
     "breakpoint",
-    :stylesheets_directory => breakpoint_path
+    :path => breakpoint_path
   )
 else
   ENV["SASS_PATH"] = [ENV["SASS_PATH"], breakpoint_path].compact.join(File::PATH_SEPARATOR)
 end
 
 module Breakpoint
-  VERSION = "2.4.4"
+  VERSION = "2.4.5"
   DATE = "2014-07-24"
 end
