0187e304a422dae1e8fa5ce02889675c4d071b03
Sam Richard <snugug@gmail.com>
2024-12-30 07:59:48 -0500

    Add skip syntax to get all tests consumed by runner
    Also re-enabled a few tests and removed their to-em bits

--- tests/index.js
+++ tests/index.js
@@ -1,18 +1,23 @@
 const sass = require('sass');
-const {readFileSync} = require('fs');
+const { readFileSync } = require('fs');
 const path = require('path');
 const glob = require('glob');
 const test = require('node:test');
 
-const tests = glob.sync(path.join(__dirname, './tests/01_pixel.scss')).filter(file => !path.basename(file).startsWith('_'));
+const tests = glob.sync(path.join(__dirname, './tests/**.scss')).filter(file => !path.basename(file).startsWith('_'));
 
 for (const t of tests) {
-  const name = path.basename(t).replace('.scss', '');
+  let name = path.basename(t).replace('.scss', '');
+  let skip = false;
+  if (name.endsWith('.skip')) {
+    skip = true;
+    name = name.replace('.skip', '')
+  }
   const expected = readFileSync(`./tests/controls/${name}.css`, 'utf-8');
 
 
-  test(name, tx => {
-    const {css} = sass.compile(t);
+  test(name, { skip }, tx => {
+    const { css } = sass.compile(t);
 
     tx.assert.strictEqual(css, expected);
   });
